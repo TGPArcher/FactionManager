827f60d64680157de61a66770acb6c71aa018037
bananasss00 <bananasss00@yandex.ru>
2018-11-04 02:37:41 +0300

    Added support [KV] Infinite Storage https://ludeon.com/forums/index.php?topic=37698.0

--- Source/Main.cs
+++ Source/Main.cs
@@ -11,6 +11,12 @@ namespace FactionManager
         {
             var harmony = HarmonyInstance.Create("TGPAcher.Rimworld.FactionManager");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            if (InfinityStorageSupport.InfinityStorageActive())
+            {
+                harmony.Patch(AccessTools.Method(AccessTools.TypeByName("InfiniteStorage.BuildingUtil"), "FindThingsOfTypeNextTo"),
+                    new HarmonyMethod(typeof(InfinityStorageSupport), nameof(InfinityStorageSupport.FindThingsOfTypeNextTo_Prefix)));
+            }
         }
     }
 }
